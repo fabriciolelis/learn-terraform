6654996b6f7e6e0d009f68e5a3cada968714398f
Fabricio Lélis <fabricio.melo@virtus.ufcg.edu.br>
2021-09-22 09:32:19 -0300

    s3 static site

---
+++ s3-bucket-static-site/variables.tf
@@ -0,0 +1,24 @@
+// Create a variable for our domain name because we'll be using it a lot.
+variable "domain_name" {
+  default = "terraform.virtus-scan-ecosystem.com"
+}
+
+variable "bucket_name" {
+  default = "terraform-test-virtus"
+}
+
+// We'll also need the root domain (also known as zone apex or naked domain).
+variable "root_domain_name" {
+  default = "virtus-scan-ecosystem.com"
+}
+
+variable "region" {
+  default     = "us-east-2"
+  description = "AWS region"
+}
+
+
+variable "tsl_certificate_arn" {
+  description = "The ARN of the certificate that the ALB uses for https"
+  default     = "arn:aws:acm:us-east-1:534327908844:certificate/1644962e-749d-4a71-8081-f1c1c41f56a1"
+}
---
+++ terraform-poc/modules/s3/variables.tf
@@ -0,0 +1,9 @@
+// Create a variable for our domain name because we'll be using it a lot.
+variable "www_domain_name" {
+  default = "www.terraform-scan-ecosystem.com"
+}
+
+// We'll also need the root domain (also known as zone apex or naked domain).
+variable "root_domain_name" {
+  default = "terraform-scan-ecosystem.com"
+}
